801d32463d5873f5aa79eefe9667f3e5d18e5f6a
vagrant <vagrant@homestead>
2019-05-02 14:45:49 +0000

    first commit

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,9 +7,17 @@ use Overtrue\Weather\Weather;
 
 class WeatherController extends Controller
 {
-    public function show(Request $request, Weather $weather, $city)
+    public function show(Request $request, $city)
     {
-        return $this->getWeather($city);
+        // 这里的 $this 有getWeather 方法？？？
+        //$w = new Weather(env('WEATHER_API_KEY'));
+        // Weather $weather 相当于 $weather = new Weather(); 你根本就没传key 为什么不报错呢？
+//         return app( 'weather')->getWeather($city, 'all', 'xml');
+
+        $weather = new Weather(env('WEATHER_API_KEY'));
+
+        return $weather->getWeather($city, 'all', 'xml');
+
     }
     
 }
